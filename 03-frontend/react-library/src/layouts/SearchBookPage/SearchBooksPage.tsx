6029db1de24a3af21588faecd3e0918f8bb8046a
alexratul <alexratul12@outlook.com>
2025-09-30 22:44:31 -0600

    Add search book page

---
+++ 03-frontend/react-library/src/layouts/SearchBookPage/SearchBooksPage.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+
+export const SearchBooksPage = () => {
+	const [books, setBooks] = useState<BookModel[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [httpError, setHttpError] = useState(null);
+};
