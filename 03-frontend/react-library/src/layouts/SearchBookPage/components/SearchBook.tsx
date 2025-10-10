a25c8a6b36837bc5aeaa75703eaf8baa4f06b03f
alexratul <alexratul12@outlook.com>
2025-10-09 21:44:57 -0600

    Fix bug on the Search book item

--- 03-frontend/react-library/src/layouts/SearchBookPage/components/SearchBook.tsx
+++ 03-frontend/react-library/src/layouts/SearchBookPage/components/SearchBook.tsx
@@ -17,10 +17,7 @@ export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
 							/>
 						)}
 					</div>
-					<div
-						className='d-lg-none d-flex justify-content-center 
-                        align-items-center'
-					>
+					<div className='d-lg-none d-flex justify-content-center align-items-center mt-3'>
 						{props.book.img ? (
 							<img src={props.book.img} width='123' height='196' alt='Book' />
 						) : (
