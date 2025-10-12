9908b4346b8338e4cc6d1dd25a6ab4cace09b87d
alexratul <alexratul12@outlook.com>
2025-10-11 21:31:09 -0600

    Implement pagination and add findByTitleContaining endpoint to the backend

--- 02-backend/spring-boot-library/src/main/java/com/alexratul/code/spring_boot_library/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/alexratul/code/spring_boot_library/dao/BookRepository.java
@@ -1,12 +1,12 @@
 package com.alexratul.code.spring_boot_library.dao;
 
-import java.math.BigInteger;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alexratul.code.spring_boot_library.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, org.springframework.data.domain.Pageable pageable);
 }
--- 03-frontend/react-library/src/layouts/SearchBookPage/SearchBooksPage.tsx
+++ 03-frontend/react-library/src/layouts/SearchBookPage/SearchBooksPage.tsx
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { SearchBook } from "./components/SearchBook";
+import { Pagination } from "../Utils/Pagination";
 
 export const SearchBooksPage = () => {
 	const [books, setBooks] = useState<BookModel[]>([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const [httpError, setHttpError] = useState(null);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [booksPerPage] = useState(5);
+	const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+	const [totalPages, setTotalPages] = useState(0);
+
 
 	useEffect(() => {
 		const fetchBooks = async () => {
@@ -15,7 +21,7 @@ export const SearchBooksPage = () => {
 			const baseUrl: string = `http://${host}:8080/api/books`;
 			console.log(baseUrl);
 
-			const url: string = `${baseUrl}?page=0&size=5`;
+			const url: string = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
 			const respose = await fetch(url);
 
 			if (!respose.ok) {
@@ -27,6 +33,8 @@ export const SearchBooksPage = () => {
 			const responseData = responseJson._embedded.books;
 
 			const loadedBooks: BookModel[] = [];
+			setTotalAmountOfBooks(responseJson.page.totalElements);
+			setTotalPages(responseJson.page.totalPages);
 
 			// console.log(loadedBooks);
 
@@ -50,7 +58,8 @@ export const SearchBooksPage = () => {
 			setIsLoading(false);
 			setHttpError(error.message);
 		});
-	}, []);
+		window.scrollTo(0, 0);
+	}, [currentPage]);
 
 	if (isLoading) {
 		return (
@@ -67,6 +76,12 @@ export const SearchBooksPage = () => {
 			</div>
 		);
 	}
+
+	const indexOfLastBook: number = currentPage * booksPerPage;
+	const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+	let lastItem = booksPerPage * currentPage < totalAmountOfBooks ? booksPerPage * currentPage : totalAmountOfBooks;
+
+	const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
 	return (
 		<div>
@@ -131,12 +146,15 @@ export const SearchBooksPage = () => {
 						</div>
 					</div>
 					<div className='mt-3'>
-						<h5>Numbers of results: (22)</h5>
+						<h5>Numbers of results: ({totalAmountOfBooks})</h5>
 					</div>
-					<p>1 to 5 of 22 items:</p>
+					<p>{indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:</p>
 					{books.map((book) => (
 						<SearchBook book={book} key={book.id}></SearchBook>
 					))}
+					{totalPages > 1 &&
+						<Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
+					}
 				</div>
 			</div>
 		</div>
