6ebac0b66a5bf7077480b0ac376d36f0cf6ea939
alexratul <alexratul12@outlook.com>
2025-10-13 20:55:07 -0600

    Add endpoint for the find by category

--- 02-backend/spring-boot-library/src/main/java/com/alexratul/code/spring_boot_library/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/alexratul/code/spring_boot_library/dao/BookRepository.java
@@ -8,5 +8,9 @@ import com.alexratul.code.spring_boot_library.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    Page<Book> findByTitleContaining(@RequestParam("title") String title, org.springframework.data.domain.Pageable pageable);
+    Page<Book> findByTitleContaining(@RequestParam("title") String title,
+            org.springframework.data.domain.Pageable pageable);
+
+    Page<Book> findByCategoryContaining(@RequestParam("category") String category,
+            org.springframework.data.domain.Pageable pageable);
 }
